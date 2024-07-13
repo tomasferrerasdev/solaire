f2f420db6878c8436c8d2909317a1d576f523dba
Tomas Ferreras <hellotomasdev@gmail.com>
2024-07-13 15:47:05 -0300

    feat: add navbar menu navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { NavbarMenu } from "@/components";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -6,10 +7,11 @@ export default function Home() {
     <main className="w-full bg-white">
       <section className="min-h-screen w-full grid grid-cols-12 gap-5">
         <article className="col-span-5 px-5">
-          <nav className="py-6">
+          <nav className="py-6 flex items-center gap-4">
             <Link href="/">
               <Image src={`/logo.svg`} alt="logo" width={40} height={40} />
             </Link>
+            <NavbarMenu />
           </nav>
         </article>
         <aside className="col-span-7">here goes a map</aside>
