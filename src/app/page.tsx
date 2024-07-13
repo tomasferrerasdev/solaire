2ddc9dc6425a4fecb3554d694ce9e762b2580350
Tomas Ferreras <hellotomasdev@gmail.com>
2024-07-13 15:13:22 -0300

    feat: add home layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="w-full bg-white">
+      <section className="min-h-screen w-full grid grid-cols-12 gap-5">
+        <article className="col-span-5 px-5">
+          <nav className="py-6">
+            <Link href="/">
+              <Image src={`/logo.svg`} alt="logo" width={40} height={40} />
+            </Link>
+          </nav>
+        </article>
+        <aside className="col-span-7">here goes a map</aside>
+      </section>
+    </main>
   );
 }
